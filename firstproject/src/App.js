adb7f2c2c22abbb6a559b21a0ed690329be7b63e
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-25 21:55:12 +0530

    basic code

---
+++ firstproject/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <h1>Hello world</h1>
+    <h1>this is the basic react project</h1>
+   </div>
+  );
+}
+
+export default App;
